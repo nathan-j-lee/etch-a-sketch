46d7ee92bc493397fea8c5e937ea03272e7ac9a4
nathan-j-lee <127238376+nathan-j-lee@users.noreply.github.com>
2023-08-17 13:27:10 -0700

    Add div grid and color on hover
    Loop over row divs and add col divs to each row div
    Container of divs sized to 1000 px for now
    When a div is hovered over, the background color of said div becomes red

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 document.getElementById('div-grid').style.display = "flex";
-for (i = 0; i < 16; i++) {
-    var div = document.createElement('div');
-    console.log(div);
-    div.className = "grid-square";
-    div.textContent='a';
-    div.style.border = "solid 1px lightgray";
-    document.getElementById('div-grid').appendChild(div);
+boxes = 24;
+for (i = 0; i < boxes; i++) {
+    // row of divs
+    var rowDiv = document.createElement('div');
+    console.log("Row created");
+    rowDiv.className = "row-div";
+    rowDiv.style.border = "solid 1px lightgray";
+    document.getElementById('div-grid').appendChild(rowDiv);
+
+    // column of divs
+    rowDiv.style.flexDirection = "column";
+    for (j = 0; j < boxes; j++) {
+        var colDiv = document.createElement('div');
+        console.log("Col created");
+        colDiv.className = "col-div";
+        colDiv.style.border = "solid 1px lightgray";
+        rowDiv.appendChild(colDiv);
+    }
+
+    // Make div red on hover
+    document.querySelectorAll('.col-div').forEach(div => {
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = "red";
+        });
+    });
+
 }
